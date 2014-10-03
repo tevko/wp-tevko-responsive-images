12366143234463a1f02da0a83be8ea55768a3c5b
Tim Evko <timevko@gmail.com>
2014-10-03 16:52:47 -0400

    separation of 1.0 and 2.0 code

---
+++ 2.0/wp-tevko-responsive-images.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * Dashboard. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/tevko/wp-tevko-responsive-images
+ * @since             2.0.0
+ * @package           http://css-tricks.com/hassle-free-responsive-images-for-wordpress/
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Tevko Responsive Images
+ * Plugin URI:        http://css-tricks.com/hassle-free-responsive-images-for-wordpress/
+ * Description:       Responsive images for wordpress using the srcset sizes syntax
+ * Version:           2.0.0
+ * Author:            Tim Evko
+ * Author URI:        http://timevko.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-tevko-responsive-images-deactivator.php';
+
+/** This action is documented in includes/class-plugin-name-deactivator.php */
+register_deactivation_hook( __FILE__, array( 'WP_Tevko_Responsive_Images_Deactivator', 'deactivate' ) );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * dashboard-specific hooks, and public-facing site hooks.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-tevko-responsive-images.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_wp_tevko_responsive_images() {
+
+	$plugin = new WP_Tevko_Responsive_Images();
+	$plugin->run();
+
+}
+
+run_wp_tevko_responsive_images();
